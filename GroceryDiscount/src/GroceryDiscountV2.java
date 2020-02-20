c7720d59d7c6cd88e0da879855397c951598b6b6
KiranTheRam <kramji12@gmail.com>
2020-02-20 12:54:15 -0500

    GroceryDiscount Project
    V1 - No loop
    V2 - While loop with NO hasnextDouble
    V3 - incorporates all aspects

---
+++ GroceryDiscount/src/GroceryDiscountV2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class GroceryDiscountV2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean keep = true;
+
+        while(keep) {
+            System.out.println("Please enter the cost of your groceries or 0 to End: ");
+            double grocCost = scan.nextDouble();
+            if (grocCost >=10 && grocCost<=1000){
+                if (grocCost < 10) {
+                    System.out.println("You are not eligible for a coupon at this time");
+                } else if (grocCost >= 10 && grocCost <= 60) {
+                    System.out.println("You win a discount coupon of $" + 0.08 * grocCost + (" (8% of your purchase)"));
+                    System.out.println();
+                } else if (grocCost > 60 && grocCost <= 150) {
+                    System.out.println("You win a discount coupon of $" + 0.1 * grocCost + (" (10% of your purchase)"));
+                    System.out.println();
+                } else if (grocCost > 150 && grocCost <= 210) {
+                    System.out.println("You win a discount coupon of $" + 0.12 * grocCost + (" (12% of your purchase)"));
+                    System.out.println();
+                } else if (grocCost > 210) {
+                    System.out.println("You win a discount coupon of $" + 0.14 * grocCost + (" (14% of your purchase)"));
+                    System.out.println();
+                }
+            }
+            else if (grocCost == 0) {
+                System.out.println("Ending...");
+//                break;
+                keep = false;
+            }
+            else {
+                System.out.println("The amount entered is not valid");
+            }
+        }
+
+    }
+}
